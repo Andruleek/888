6d47192a07fa5951eed848152e50327fd296f109
ExtymAndriy <150171957+ExtymAndriy@users.noreply.github.com>
2024-07-01 17:59:24 +0300

    2

---
+++ hw_10/users/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render, redirect
+from users.forms import RegisterForm
+from django.contrib.auth import login, authenticate
+
+def register(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            return redirect('main')
+    else:
+        form = RegisterForm()
+    return render(request, 'users/register.html', {'form': form})
